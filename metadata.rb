78a8c3a3608d00c5e7bc6a23a5a99803a7762735
Kidhar Bachan <Kidhar.Bachan@absa.co.za>
2017-08-15 15:10:54 +0200

    Replaced wlp with was_liberty

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email ''
 license          'All Rights Reserved'
 description      'Installs WebSphere Application Server Liberty Profile'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version          '1.0.2'
 
 supports "aix"
 supports "debian"
